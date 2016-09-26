3fe4c5ba1d385c81e39e261a521970cc68336ff5
Tim Lovorn <tflovorn@crimson.ua.edu>
2016-09-26 14:20:15 -0500

    construct slab calculation

--- mkheusler/build/slab.py
+++ mkheusler/build/slab.py
@@ -2,98 +2,11 @@ import argparse
 import os
 import numpy as np
 from ase.lattice import bulk
+from ase.build import surface
 from mkheusler.pwscf.build import build_pw2wan, build_bands, build_qe
 from mkheusler.wannier.build import Winfile
 from mkheusler.build.util import _base_dir, _global_config
-
-def verify_SC10_fcc(system, a):
-    SC10_fcc = np.array([[0.0, a/2, a/2],
-        [a/2, 0.0, a/2],
-        [a/2, a/2, 0.0]])
-
-    if not np.array_equal(system.cell, SC10_fcc):
-        raise ValueError("system does not follow Setyawan FCC cell convention")
-
-def get_num_bands(system, system_type, atoms, soc):
-    # TODO may want to revise output from this fn based on projections
-    syms = system.get_chemical_symbols()
-    if system_type == "HH":
-        wann_valence = {atoms[0]: 9, atoms[1]: 9, atoms[2]: 4}
-    elif system_type == "FH":
-        # for FH, atoms[0] = atoms[1] (TODO - check here?)
-        wann_valence = {atoms[0]: 9, atoms[2]: 9, atoms[3]: 4}
-    else:
-        raise ValueError("found unexpected system_type")
-
-    num_wann = 0
-    for sym in syms:
-        if sym in wann_valence:
-            num_wann += wann_valence[sym]
-        else:
-            raise ValueError("found unexpected atomic symbol")
-
-    if soc:
-        num_wann *= 2
-
-    extra_bands_factor = 28/18
-    num_bands = int(np.ceil(extra_bands_factor*num_wann))
-    if num_bands % 2 == 1:
-        num_bands += 1
-
-    return num_wann, num_bands
-
-def get_weight(system):
-    syms = system.get_chemical_symbols()
-    all_weights = system.get_masses()
-
-    weight = {}
-    for sym, sym_weight in zip(syms, all_weights):
-        weight[sym] = sym_weight
-
-    return weight
-
-def get_pseudo(syms):
-    pseudo = {}
-    # TODO want to search pseudo_dir instead?
-    for sym in syms:
-        pseudo[sym] = "{}.UPF".format(sym)
-
-    return pseudo
-
-def get_cutoff(ecutwfc_in, ecutrho_in):
-    if ecutwfc_in is None:
-        ecutwfc = 60.0
-    else:
-        ecutwfc = args.ecutwfc
-
-    if ecutrho_in is None:
-        ecutrho = 240.0
-    else:
-        ecutrho = args.ecutrho
-
-    return ecutwfc, ecutrho
-
-def make_qe_config(system, latconst, soc, num_bands, ecutwfc, ecutrho, degauss, Nk, band_path):
-    base = _base_dir()
-    if soc:
-        pseudo_dir = os.path.join(base, "pseudo", "SG15", "soc")
-    else:
-        pseudo_dir = os.path.join(base, "pseudo", "SG15", "base_no_soc")
-
-    pseudo = get_pseudo(system.get_chemical_symbols())
-    weight = get_weight(system)
-    conv_thr = {"scf": 1e-8, "nscf": 1e-10, "bands": 1e-10}
-
-    qe_config = {"pseudo_dir": pseudo_dir, "pseudo": pseudo, "soc": soc, "latconst": latconst, 
-            "num_bands": num_bands, "weight": weight, "ecutwfc": ecutwfc, "ecutrho": ecutrho,
-            "degauss": degauss, "conv_thr": conv_thr, "Nk": Nk, "band_path": band_path}
-
-    return qe_config
-
-def write_qe_input(prefix, file_dir, qe_input, calc_type):
-    file_path = os.path.join(file_dir, "{}.{}.in".format(prefix, calc_type))
-    with open(file_path, 'w') as fp:
-        fp.write(qe_input[calc_type])
+from mkheusler.build.bulk import verify_SC10_fcc, get_num_bands, get_cutoff, make_qe_config, write_qe_input
 
 def _main():
     parser = argparse.ArgumentParser("Build and run Heusler bulk",
@@ -102,6 +15,8 @@ def _main():
             help="Comma-separated list of XYZ/X2YZ atoms (ex: 'Ni,Mn,Sb' or 'Co,Co,Mn,Si')")
     parser.add_argument("latconst", type=float,
             help="Lattice constant in Ang (conventional cell cubic edge)")
+    parser.add_argument("layers", type=int,
+            help="Number of layers in the slab")
     parser.add_argument("--prefix", type=str, default=None,
             help="System name (obtained from atoms if not specified)")
     parser.add_argument("--soc", action="store_true",
@@ -113,9 +28,9 @@ def _main():
     parser.add_argument("--degauss", type=float, default=0.02,
             help="Delta-function smearing constant (Ry)")
     parser.add_argument("--Nk_scf", type=int, default=16,
-            help="Number of k-points to use in SCF calculation (same in each direction)")
+            help="Number of k-points to use in SCF calculation (same in each direction along slab)")
     parser.add_argument("--Nk_nscf", type=int, default=8,
-            help="Number of k-points to use in NSCF calculation (same in each direction)")
+            help="Number of k-points to use in NSCF calculation (same in each direction along slab)")
     parser.add_argument("--Nk_bands", type=int, default=20,
             help="Number of k-points to use for each panel in bands calculation")
     args = parser.parse_args()
@@ -125,13 +40,14 @@ def _main():
     # NOTE - assuming no SOC = SG15 and SOC = SG15 plus SOC.
     ecutwfc, ecutrho = get_cutoff(args.ecutwfc, args.ecutrho)
 
+    # TODO include growth dir in prefix?
     atoms = args.atoms.split(',')
     if len(atoms) == 3:
         system_type = "HH"
         wann_valence = {atoms[0]: "spd", atoms[1]: "spd", atoms[2]: "sp"}
 
         if args.prefix is None:
-            prefix = "{}{}{}_bulk".format(atoms[0], atoms[1], atoms[2])
+            prefix = "{}{}{}_slab_{}".format(atoms[0], atoms[1], atoms[2], args.layers)
             if args.soc:
                 prefix = "{}_soc".format(prefix)
     elif len(atoms) == 4:
@@ -139,14 +55,14 @@ def _main():
         wann_valence = {atoms[0]: "spd", atoms[2]: "spd", atoms[3]: "sp"}
 
         if args.prefix is None:
-            prefix = "{}2{}{}_bulk".format(atoms[0], atoms[2], atoms[3])
+            prefix = "{}2{}{}_slab_{}".format(atoms[0], atoms[2], atoms[3], args.layers)
             if args.soc:
                 prefix = "{}_soc".format(prefix)
     else:
         raise ValueError("must specify 3 or 4 atoms (half-Heusler or full-Heusler)")
 
-    system = bulk(atoms, 'fcc', a=args.latconst)
-    verify_SC10_fcc(system, args.latconst)
+    system_bulk = bulk(atoms, 'fcc', a=args.latconst)
+    verify_SC10_fcc(system_bulk, args.latconst)
 
     # SC10 = Setyawan and Curtarolo, Comp. Mater. Sci. 49, 299 (2010).
     # SC10 FCC cell 111 = cubic conventional cell 111
@@ -154,18 +70,24 @@ def _main():
     # cubic conventional cell.
     if system_type == "HH":
         # Half-Heusler: Y-X-Z-void
-        system.set_scaled_positions([[1/4, 1/4, 1/4],
+        system_bulk.set_scaled_positions([[1/4, 1/4, 1/4],
                 [0.0, 0.0, 0.0],
                 [1/2, 1/2, 1/2]])
     elif system_type == "FH":
         # Full-Heusler: Y-X-Z-X
-        system.set_scaled_positions([[1/4, 1/4, 1/4],
+        system_bulk.set_scaled_positions([[1/4, 1/4, 1/4],
             [3/4, 3/4, 3/4],
             [0.0, 0.0, 0.0],
             [1/2, 1/2, 1/2]])
 
-    num_wann, num_bands = get_num_bands(system, system_type, atoms, args.soc)
+    surface_normal_cubic = (1, 1, 1)
+    surface_normal_fcc = (1, 1, 1) # TODO convert from cubic to fcc system (111 is the same)
+    vacuum = 20 # Angstrom
+    system_slab = surface(system_bulk, surface_normal_fcc, args.layers, vacuum)
 
+    num_wann, num_bands = get_num_bands(system_slab, system_type, atoms, args.soc)
+
+    # TODO (111) surface
     band_path_syms = ["Gamma", "X", "W", "K", "Gamma", "L", "U", "W", "L", "K", "W", "U", "X"]
     SC10_kpts = {"Gamma": np.array([0.0, 0.0, 0.0]),
             "K": np.array([3/8, 3/8, 3/4]),
@@ -176,12 +98,12 @@ def _main():
     band_path = [SC10_kpts[sym] for sym in band_path_syms]
 
     Nk = {"scf": args.Nk_scf, "nscf": args.Nk_nscf, "bands": args.Nk_bands}
-    qe_config = make_qe_config(system, args.latconst, args.soc, num_bands, ecutwfc,
+    qe_config = make_qe_config(system_slab, args.latconst, args.soc, num_bands, ecutwfc,
             ecutrho, args.degauss, Nk, band_path)
 
     qe_input = {}
     for calc_type in ["scf", "nscf", "bands"]:
-        qe_input[calc_type] = build_qe(system, prefix, calc_type, qe_config)
+        qe_input[calc_type] = build_qe(system_slab, prefix, calc_type, qe_config)
 
     work = os.path.join(_global_config()["work_base"], prefix)
     if not os.path.exists(work):
@@ -209,7 +131,7 @@ def _main():
     with open(bands_post_path, 'w') as fp:
         fp.write(bands_post_input)
 
-    wannier_input = Winfile(system, qe_config, wann_valence, num_wann)
+    wannier_input = Winfile(system_slab, qe_config, wann_valence, num_wann)
     win_path = os.path.join(wannier_dir, "{}.win".format(prefix))
     with open(win_path, 'w') as fp:
         fp.write(wannier_input)
